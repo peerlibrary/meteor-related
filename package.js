55833435e4472ead55191fde7d6eee94dd964b14
Mitar <mitar.git@tnode.com>
2014-09-13 10:55:24 -0700

    Transitory package.

--- package.js
+++ package.js
@@ -1,21 +1,10 @@
 Package.describe({
   summary: "Publish with reactive dependencies on related queries",
   version: '0.1.5',
-  name: 'peerlibrary:related',
+  name: 'mrt:related',
   git: 'https://github.com/peerlibrary/meteor-related.git'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.1.1');
-  api.use(['coffeescript', 'underscore', 'peerlibrary:assert@0.2.5'], 'server');
-
-  api.add_files([
-    'server.coffee'
-  ], 'server');
-});
-
-Package.on_test(function (api) {
-  api.use(['peerlibrary:related', 'tinytest', 'test-helpers', 'coffeescript', 'insecure', 'random'], ['client', 'server']);
-
-  api.add_files('tests.coffee', ['client', 'server']);
+  api.imply('peerlibrary:related@0.1.5');
 });
